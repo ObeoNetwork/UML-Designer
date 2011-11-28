bf0bc5a6909280aac4ec30984b4d013e24f4796f
cbrun <cedric.brun@obeo.fr>
2011-11-28 17:32:35 +0100

    fixing license header mismatch during rebase

--- plugins/org.obeonetwork.dsl.uml2.design/src/org/obeonetwork/dsl/uml2/design/services/UMLProfileServices.java
+++ plugins/org.obeonetwork.dsl.uml2.design/src/org/obeonetwork/dsl/uml2/design/services/UMLProfileServices.java
@@ -1,21 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2009 Obeo. All Rights Reserved.
- *
- * This software and the attached documentation are the exclusive ownership
- * of its authors and was conceded to the profit of Obeo SARL.
- * This software and the attached documentation are protected under the rights
- * of intellectual ownership, including the section "Titre II  Droits des auteurs (Articles L121-1 L123-12)"
- * By installing this software, you acknowledge being aware of this rights and
- * accept them, and as a consequence you must:
- * - be in possession of a valid license of use conceded by Obeo only.
- * - agree that you have read, understood, and will comply with the license terms and conditions.
- * - agree not to do anything that could conflict with intellectual ownership owned by Obeo or its beneficiaries
- * or the authors of this software
- *
- * Should you not agree with these terms, you must stop to use this software and give it back to its legitimate owner.
- *
- * Acceleo and Obeo are trademarks owned by Obeo.
- *
+ * Copyright (c) 2011 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
  *******************************************************************************/
 package org.obeonetwork.dsl.uml2.design.services;
 
