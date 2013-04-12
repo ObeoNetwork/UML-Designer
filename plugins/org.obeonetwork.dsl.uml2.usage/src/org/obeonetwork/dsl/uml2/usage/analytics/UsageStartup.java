86725e0a6a85c63c4f2e434fbfa58002c85026db
Mélanie Bats <melanie.bats@obeo.fr>
2013-04-12 08:33:36 +0200

    Move usage dialog later when the first Sirius editor is opened

--- plugins/org.obeonetwork.dsl.uml2.usage/src/org/obeonetwork/dsl/uml2/usage/analytics/UsageStartup.java
+++ plugins/org.obeonetwork.dsl.uml2.usage/src/org/obeonetwork/dsl/uml2/usage/analytics/UsageStartup.java
@@ -10,14 +10,10 @@
  *******************************************************************************/
 package org.obeonetwork.dsl.uml2.usage.analytics;
 
-import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IStartup;
 import org.eclipse.ui.IWindowListener;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
-import org.obeonetwork.dsl.uml2.usage.dialog.UsageDialog;
-import org.obeonetwork.dsl.uml2.usage.preferences.UsagePreferences;
 
 /**
  * Activates listener on SIrius editors at startup.
@@ -35,22 +31,9 @@ public class UsageStartup implements IStartup {
 	private SiriusEditorsListener usageListener = null;
 
 	/**
-	 * Usage preferences.
+	 * Called at Eclipse startup.
 	 */
-	private UsagePreferences preferences = new UsagePreferences();
-
 	public void earlyStartup() {
-		Display.getDefault().asyncExec(new Runnable() {
-
-			public void run() {
-				try {
-					askUser();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-
 		PlatformUI.getWorkbench().addWindowListener(new IWindowListener() {
 
 			public void windowOpened(IWorkbenchWindow window) {
@@ -74,29 +57,6 @@ public class UsageStartup implements IStartup {
 			}
 
 		});
-	}
-
-	/**
-	 * Ask to user if it is ok to send some statistics about its usage of UML
-	 * Designer.
-	 */
-	private void askUser() {
-		// Check preference to see if user already answer the question
-		if (preferences.hasAnswered()) {
-			return;
-		}
-
-		// User does not answer to the question, ask him
-		Shell shell = PlatformUI.getWorkbench().getModalDialogShellProvider()
-				.getShell();
-		UsageDialog dialog = new UsageDialog(shell);
-		int answer = dialog.open();
-
-		// Set the user answer to the preference store in order to not ask him
-		// anymore if he answered. If the user just close the dialog, the
-		// question will be ask again at next startup. The user can update its
-		// answer at any time in the preferences page.
-		preferences.storeUserAnswer(answer);
 	}
 
 }
