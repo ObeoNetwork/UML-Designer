5d6dadc38bebc0916173e396e9b2f4eac53e0f94
Hugo Marchadour <hugo.marchadour@obeo.fr>
2013-09-23 18:16:12 +0200

    UML-360: Add a reconnect tool on binding edge for Class Template.

--- plugins/org.obeonetwork.dsl.uml2.design/src/org/obeonetwork/dsl/uml2/design/services/internal/ReconnectSwitch.java
+++ plugins/org.obeonetwork.dsl.uml2.design/src/org/obeonetwork/dsl/uml2/design/services/internal/ReconnectSwitch.java
@@ -15,6 +15,7 @@ import java.util.List;
 import org.eclipse.uml2.uml.Association;
 import org.eclipse.uml2.uml.Class;
 import org.eclipse.uml2.uml.Classifier;
+import org.eclipse.uml2.uml.ClassifierTemplateParameter;
 import org.eclipse.uml2.uml.Component;
 import org.eclipse.uml2.uml.ComponentRealization;
 import org.eclipse.uml2.uml.ConnectableElement;
@@ -27,16 +28,26 @@ import org.eclipse.uml2.uml.ExtensionEnd;
 import org.eclipse.uml2.uml.Generalization;
 import org.eclipse.uml2.uml.Interface;
 import org.eclipse.uml2.uml.InterfaceRealization;
+import org.eclipse.uml2.uml.NamedElement;
+import org.eclipse.uml2.uml.ParameterableElement;
 import org.eclipse.uml2.uml.Property;
 import org.eclipse.uml2.uml.Stereotype;
+import org.eclipse.uml2.uml.TemplateBinding;
+import org.eclipse.uml2.uml.TemplateParameter;
+import org.eclipse.uml2.uml.TemplateParameterSubstitution;
+import org.eclipse.uml2.uml.TemplateSignature;
+import org.eclipse.uml2.uml.TemplateableElement;
 import org.eclipse.uml2.uml.Type;
+import org.eclipse.uml2.uml.UMLFactory;
 import org.eclipse.uml2.uml.util.UMLSwitch;
+import org.obeonetwork.dsl.uml2.design.services.LogServices;
 import org.obeonetwork.dsl.uml2.design.services.UMLServices;
 
 /**
  * A switch that handle the edge reconnections for each UML types.
  * 
  * @author Stephane Thibaudeau <a href="mailto:stephane.thibaudeau@obeo.fr">stephane.thibaudeau@obeo.fr</a>
+ * @author Hugo Marchadour <a href="mailto:hugo.marchadour@obeo.fr">hugo.marchadour@obeo.fr</a>
  */
 public class ReconnectSwitch extends UMLSwitch<Element> {
 	/**
@@ -222,5 +233,156 @@ public class ReconnectSwitch extends UMLSwitch<Element> {
 			}
 		}
 		return connector;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Element caseTemplateBinding(TemplateBinding tmplBinding) {
+		if (newPointedElement instanceof TemplateableElement
+				&& oldPointedElement instanceof TemplateableElement) {
+			if (RECONNECT_SOURCE == reconnectKind) {
+				TemplateableElement templateableElement = (TemplateableElement)newPointedElement;
+				tmplBinding.setBoundElement(templateableElement);
+			} else {
+				final TemplateableElement oldTmplElement = (TemplateableElement)oldPointedElement;
+				final TemplateableElement newTmplElement = (TemplateableElement)newPointedElement;
+				TemplateSignature oldTemplate = oldTmplElement.getOwnedTemplateSignature();
+				TemplateSignature newTemplate = newTmplElement.getOwnedTemplateSignature();
+				if (newTemplate != null) {
+					List<TemplateParameter> newParams = newTemplate.getOwnedParameters();
+					List<TemplateParameter> oldParams = oldTemplate.getOwnedParameters();
+					tmplBinding.setSignature(newTemplate);
+
+					if (newParams.size() > oldParams.size()) {
+						handleHigherTmplParamCardinality(tmplBinding);
+					} else if (newParams.size() < oldParams.size()) {
+						handleLowerTmplParamCardinality(tmplBinding, oldTmplElement, newTmplElement);
+					} else {
+						handleSameTmplParamCardinality(tmplBinding);
+					}
+				} else {
+					createANewTemplateSignature(tmplBinding, oldTmplElement, newTmplElement);
+				}
+			}
+		}
+		return tmplBinding;
+	}
+
+	/**
+	 * The new TemplateableElement hasn't a template signature. We need to create with the same signature as
+	 * on the old TemplateableElement and update the binding.
+	 * 
+	 * @param tmplBinding
+	 * @param oldTmplElement
+	 * @param newTmplElement
+	 */
+	private void createANewTemplateSignature(TemplateBinding tmplBinding, TemplateableElement oldTmplElement,
+			TemplateableElement newTmplElement) {
+		TemplateSignature templateSignature = newTmplElement.createOwnedTemplateSignature();
+		tmplBinding.setSignature(templateSignature);
+		List<TemplateParameter> oldParams = oldTmplElement.getOwnedTemplateSignature().getOwnedParameters();
+		for (TemplateParameter templateParameter : oldParams) {
+			ClassifierTemplateParameter newTemplateClassifier = UMLFactory.eINSTANCE
+					.createClassifierTemplateParameter();
+			Class newGenericClass = UMLFactory.eINSTANCE.createClass();
+			ParameterableElement ownedDefault = templateParameter.getOwnedDefault();
+			if (ownedDefault instanceof NamedElement) {
+				newGenericClass.setName(((NamedElement)ownedDefault).getName());
+			}
+			newTemplateClassifier.setOwnedDefault(newGenericClass);
+			newTemplateClassifier.setParameteredElement(newGenericClass);
+
+			templateSignature.getOwnedParameters().add(newTemplateClassifier);
+		}
+		// Adapt the binding associations
+		handleSameTmplParamCardinality(tmplBinding);
+	}
+
+	/**
+	 * The new TemplateableElement has the same number of TemplateParameter. For each parameterSubstitutions,
+	 * we need to update the formal value with the related TemplateParameter.
+	 * 
+	 * @param tmplBinding
+	 * @param newParams
+	 */
+	private void handleSameTmplParamCardinality(TemplateBinding tmplBinding) {
+		// Adapt the binding association with the newParams
+		TemplateSignature newTmplSignature = tmplBinding.getSignature();
+		List<TemplateParameterSubstitution> parameterSubstitutions = tmplBinding.getParameterSubstitutions();
+		int i = 0;
+		for (TemplateParameterSubstitution templateParameterSubstitution : parameterSubstitutions) {
+			if (i < newTmplSignature.getOwnedParameters().size()) {
+				templateParameterSubstitution.setFormal(newTmplSignature.getOwnedParameters().get(i));
+			} else {
+				// the model is dirty
+				new LogServices().error("ReconnectSwitch.caseTemplateBinding(" + tmplBinding.getClass()
+						+ ") dirty TemplateBinding model.", null);
+				break;
+			}
+			i++;
+		}
+	}
+
+	/**
+	 * The new TemplateableElement has a higher number of TemplateParameter. For each parameterSubstitutions,
+	 * we need to update the formal value with the related TemplateParameter. In a second time, we need to add
+	 * missing parameterSubstitutions.
+	 * 
+	 * @param tmplBinding
+	 * @param newParams
+	 */
+	private void handleHigherTmplParamCardinality(TemplateBinding tmplBinding) {
+		// We adapt the binding association
+		TemplateSignature newTmplSignature = tmplBinding.getSignature();
+		List<TemplateParameterSubstitution> paramSubs = tmplBinding.getParameterSubstitutions();
+		for (int i = 0; i < newTmplSignature.getOwnedParameters().size(); i++) {
+			TemplateParameterSubstitution tmplParamSub;
+			if (i < paramSubs.size()) {
+				tmplParamSub = paramSubs.get(i);
+			} else {
+				tmplParamSub = tmplBinding.createParameterSubstitution();
+			}
+			tmplParamSub.setFormal(newTmplSignature.getOwnedParameters().get(i));
+		}
+	}
+
+	/**
+	 * The new TemplateableElement has a lower number of TemplateParameter. firstly, we add missing
+	 * TemplateParameters. Secondly, for each parameterSubstitutions, we need to update the formal value with
+	 * the related TemplateParameter.
+	 * 
+	 * @param tmplBinding
+	 * @param newParams
+	 */
+	private void handleLowerTmplParamCardinality(TemplateBinding tmplBinding,
+			TemplateableElement oldTmplElement, TemplateableElement newTmplElement) {
+
+		TemplateSignature oldTemplate = oldTmplElement.getOwnedTemplateSignature();
+		TemplateSignature newTemplate = newTmplElement.getOwnedTemplateSignature();
+
+		List<TemplateParameter> newParams = newTemplate.getOwnedParameters();
+		List<TemplateParameter> oldParams = oldTemplate.getOwnedParameters();
+
+		for (int i = newParams.size(); i < oldParams.size(); i++) {
+			ClassifierTemplateParameter newTemplateClassifier = UMLFactory.eINSTANCE
+					.createClassifierTemplateParameter();
+			Class newGenericClass = UMLFactory.eINSTANCE.createClass();
+			ParameterableElement ownedDefault = oldParams.get(i).getOwnedDefault();
+			if (ownedDefault instanceof NamedElement) {
+				newGenericClass.setName(((NamedElement)ownedDefault).getName());
+			}
+			newTemplateClassifier.setOwnedDefault(newGenericClass);
+			newTemplateClassifier.setParameteredElement(newGenericClass);
+
+			newParams.add(newTemplateClassifier);
+
+		}
+		List<TemplateParameterSubstitution> paramSubs = tmplBinding.getParameterSubstitutions();
+		for (int i = 0; i < newParams.size(); i++) {
+			TemplateParameterSubstitution tmplParamSub = paramSubs.get(i);
+			tmplParamSub.setFormal(newParams.get(i));
+		}
 	}
 }
