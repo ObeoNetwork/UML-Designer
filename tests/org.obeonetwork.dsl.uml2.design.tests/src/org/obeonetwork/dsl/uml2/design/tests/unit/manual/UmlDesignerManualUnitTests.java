d2e1977aac18b6bbbca35bf094d615af2590c3d7
Mélanie Bats <melanie.bats@obeo.fr>
2014-04-03 12:01:22 +0200

    Fix #440 Smart direct edit on properties
    Smart direct Edit : type "name:String", "x", "x y", "x..y", "[x]", "[x y]", "[x..y]", "x..*", "[x..*]", "*", "[*]", "-1", "[-1]", ":Integer" and the modeler will figure what you meant.

--- tests/org.obeonetwork.dsl.uml2.design.tests/src/org/obeonetwork/dsl/uml2/design/tests/unit/manual/UmlDesignerManualUnitTests.java
+++ tests/org.obeonetwork.dsl.uml2.design.tests/src/org/obeonetwork/dsl/uml2/design/tests/unit/manual/UmlDesignerManualUnitTests.java
@@ -16,7 +16,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({})
+@SuiteClasses({SmartEditPropertyUnitTests.class})
 /**
  * Testing : Hand written UML Designer unit tests
  */
