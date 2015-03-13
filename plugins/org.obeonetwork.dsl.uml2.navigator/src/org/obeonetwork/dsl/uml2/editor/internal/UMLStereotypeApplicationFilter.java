5d155ec20215d5bc21b401f0cfd9269de7254568
Cedric Notot <cedric.notot@obeo.fr>
2015-03-13 23:58:01 +0100

    [526] Stereotype applications shouldn't be visible in the model explorer
    As now, the stereotype applications are hidden. It is possible to see
    them again using the "Customize View.../Filters" action of the standard
    Eclipse View Menu and unchecking the "UML Stereotype applications"
    option.

---
+++ plugins/org.obeonetwork.dsl.uml2.navigator/src/org/obeonetwork/dsl/uml2/editor/internal/UMLStereotypeApplicationFilter.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Obeo.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Obeo - initial API and implementation
+ *******************************************************************************/
+package org.obeonetwork.dsl.uml2.editor.internal;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.jface.viewers.ViewerFilter;
+
+/**
+ * Filter to enable the user to hide Stereotype applications displayed as
+ * children of UML resources.
+ * 
+ * @author <a href="mailto:cedric.notot@obeo.fr">Cédric Notot</a>
+ * 
+ */
+public class UMLStereotypeApplicationFilter extends ViewerFilter {
+
+	public UMLStereotypeApplicationFilter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean select(Viewer viewer, Object parentElement, Object element) {
+		return !(element instanceof EObject 
+				&& ((EObject)element).eContainer() == null
+				&& containsBaseReference(((EObject)element).eClass().getEAllReferences()));
+	}
+	
+	private boolean containsBaseReference(List<EReference> references) {
+		for (EReference eReference : references) {
+			if (eReference.getName().startsWith("base_")) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
