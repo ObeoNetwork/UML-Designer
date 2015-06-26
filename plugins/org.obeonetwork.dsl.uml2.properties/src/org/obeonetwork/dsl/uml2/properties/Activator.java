f9fffa38f93281aeeef3df55cbfd9ce88e838b92
Mélanie Bats <melanie.bats@obeo.fr>
2015-06-26 11:22:00 +0200

    Temporary workaround to force EEF plugin properties and eef.adapters activation at startup
    The UML Designer EEF properties plugin is activated at startup. Then from the
    start  method we call a method of the EEF adapetrs plugin to force the plugin
    activation.

--- plugins/org.obeonetwork.dsl.uml2.properties/src/org/obeonetwork/dsl/uml2/properties/Activator.java
+++ plugins/org.obeonetwork.dsl.uml2.properties/src/org/obeonetwork/dsl/uml2/properties/Activator.java
@@ -40,6 +40,8 @@ public class Activator extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		// Call a method of the eef adapters just to activate the plugin
+		org.eclipse.sirius.eef.adapters.Activator.getDefault();
 	}
 
 	/**
