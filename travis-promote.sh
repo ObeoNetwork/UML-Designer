e18377fcd4bddff4f92b95f5ba24873a4043d3c8
Mélanie Bats <melanie.bats@obeo.fr>
2015-07-06 16:59:26 +0200

    Update Travis build
    With Travis, we :
    * generate the documentation
    * copy the documentation to the eclipse help plugin
    * build the nightly update-site
    * build the nightly bundles
    * promote the nightly update-site, target platform and bundles under s3
    * if a tag is set the deployment releases the build on s3

--- travis-promote.sh
+++ travis-promote.sh
@@ -3,6 +3,7 @@ echo "Build $TRAVIS_JOB_NUMBER"
 echo "Git: $TRAVIS_COMMIT [$TRAVIS_BRANCH]"
 echo "Root dir: $TRAVIS_BUILD_DIR"
 cd $TRAVIS_BUILD_DIR
+echo "Promote umldesigner.org web site -- Start"
 echo "Clone UML Designer repo"
 git clone https://$GITHUB_TOKEN@github.com/ObeoNetwork/UML-Designer.git -b gh-pages --quiet
 cd UML-Designer/
@@ -13,9 +14,13 @@ echo "Copy new generated web site"
 cp -r ../documentation/_site/* .
 ls
 rm -rf ../documentation/_site/
+echo "Promote umldesigner.org web site -- End"
 git config user.email "melanie.bats@obeo.fr"
 git config user.name "Mélanie Bats"
+git status
 git add .
-git commit -m "Promoting a new website for https://github.com/ObeoNetwork/UML-Designer/commit/$TRAVIS_COMMIT [$TRAVIS_BRANCH]"
+git commit -m "Promoting new website for https://github.com/ObeoNetwork/UML-Designer/commit/$TRAVIS_COMMIT [$TRAVIS_BRANCH]"
+git status
 git push origin gh-pages --quiet &>/dev/null
+
 echo "Build promoted."
