f3ed4bd472903c137ecc18c2b7f182552fa77b6e
Axel RICHARD <axel.richard@obeo.fr>
2016-09-08 15:25:23 +0200

    Handle BorderedNode in rotatable node gmf extension plugin (#886)
    Change-Id: I47a211f7bd1b87ea26098ecd98c89f0a2be6ad82

--- plugins/org.obeonetwork.gmf.runtime.diagram.ui.extension/src/org/obeonetwork/gmf/runtime/diagram/ui/extension/rotatable/provider/RotatableImageEditPartProvider.java
+++ plugins/org.obeonetwork.gmf.runtime.diagram.ui.extension/src/org/obeonetwork/gmf/runtime/diagram/ui/extension/rotatable/provider/RotatableImageEditPartProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 Obeo.
+ * Copyright (c) 2013, 2016 Obeo.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -16,11 +16,16 @@ import java.util.List;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.gmf.runtime.diagram.core.util.ViewUtil;
 import org.eclipse.gmf.runtime.diagram.ui.services.editpart.AbstractEditPartProvider;
+import org.eclipse.gmf.runtime.draw2d.ui.graph.BorderNode;
 import org.eclipse.gmf.runtime.notation.Node;
 import org.eclipse.gmf.runtime.notation.View;
 import org.eclipse.sirius.diagram.CustomStyle;
+import org.eclipse.sirius.diagram.DNode;
+import org.eclipse.sirius.diagram.DiagramFactory;
+import org.eclipse.sirius.diagram.DiagramPackage;
 import org.obeonetwork.gmf.runtime.diagram.ui.extension.ExtensionActivator;
 import org.obeonetwork.gmf.runtime.diagram.ui.extension.rotatable.NodeImageExtension;
+import org.obeonetwork.gmf.runtime.diagram.ui.extension.rotatable.editPart.RotatableBorderedNodeEditPart;
 import org.obeonetwork.gmf.runtime.diagram.ui.extension.rotatable.editPart.RotatableImageBasedOnFirstSourceConnectionEditPart;
 import org.obeonetwork.gmf.runtime.diagram.ui.extension.rotatable.editPart.RotatableNodeEditPart;
 
@@ -29,6 +34,7 @@ import org.obeonetwork.gmf.runtime.diagram.ui.extension.rotatable.editPart.Rotat
  * 
  * @author nlepine
  * @author hmarchadour
+ * @author arichard
  */
 public class RotatableImageEditPartProvider extends AbstractEditPartProvider {
 
@@ -42,6 +48,9 @@ public class RotatableImageEditPartProvider extends AbstractEditPartProvider {
 		if (semanticElement instanceof CustomStyle) {
 			final CustomStyle customStyle = (CustomStyle)semanticElement;
 			if (customStyleSupported(customStyle)) {
+				if (isBorderedNode(semanticElement)) {
+					return RotatableBorderedNodeEditPart.class;
+				}
 				return RotatableImageBasedOnFirstSourceConnectionEditPart.class;
 			}
 		} else if (view instanceof Node) {
@@ -52,13 +61,27 @@ public class RotatableImageEditPartProvider extends AbstractEditPartProvider {
 					if (childSemanticElement instanceof CustomStyle) {
 						final CustomStyle customStyle = (CustomStyle)childSemanticElement;
 						if (customStyleSupported(customStyle)) {
-							return RotatableNodeEditPart.class;
+							if (!isBorderedNode(semanticElement)) {
+								return RotatableNodeEditPart.class;
+							}
 						}
 					}
 				}
 			}
 		}
 		return super.getNodeEditPartClass(view);
+	}
+
+	private boolean isBorderedNode(EObject object) {
+		if (object instanceof CustomStyle) {
+			EObject container = object.eContainer();
+			if (container instanceof DNode) {
+				return isBorderedNode(container);
+			}
+		} else if (DiagramPackage.Literals.ABSTRACT_DNODE__OWNED_BORDERED_NODES.equals(object.eContainingFeature())) {
+			return true;
+		}
+		return false;
 	}
 
 	private boolean customStyleSupported(CustomStyle customStyle) {
