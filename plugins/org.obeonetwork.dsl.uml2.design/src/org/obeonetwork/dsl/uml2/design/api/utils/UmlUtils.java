9c575e4bd8ae77131bf9cf3dab8289a2baca9501
Axel Richard <axel.richard@obeo.fr>
2017-01-26 10:00:16 +0100

    Fix #906 [Profile] Can't delete profile from Model Explorer Tree View
    A ConncurrentModificationException was raising while deleting an applied
    profile.
    
    Change-Id: Ia77c1b0de0866b83b1e523fa9be50ee70e9f1f4d
    Signed-off-by: Axel Richard <axel.richard@obeo.fr>

--- plugins/org.obeonetwork.dsl.uml2.design/src/org/obeonetwork/dsl/uml2/design/api/utils/UmlUtils.java
+++ plugins/org.obeonetwork.dsl.uml2.design/src/org/obeonetwork/dsl/uml2/design/api/utils/UmlUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2015 Obeo.
+ * Copyright (c) 2015, 2017 Obeo.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -61,7 +61,7 @@ public class UmlUtils {
 		} else {
 
 			for (final TreeIterator<EObject> allContents = UML2Util.getAllContents(ancestorEObject, true,
-					false); allContents.hasNext();) {
+					true); allContents.hasNext();) {
 
 				final EObject eObject = allContents.next();
 
@@ -71,7 +71,7 @@ public class UmlUtils {
 			}
 
 			for (final TreeIterator<EObject> allContents = UML2Util.getAllContents(ancestorEObject, true,
-					false); allContents.hasNext();) {
+					true); allContents.hasNext();) {
 
 				final EObject eObject = allContents.next();
 
@@ -83,7 +83,7 @@ public class UmlUtils {
 			}
 
 			for (final TreeIterator<EObject> allContents = UML2Util.getAllContents(ancestorEObject, true,
-					false); allContents.hasNext();) {
+					true); allContents.hasNext();) {
 
 				final EObject eObject = allContents.next();
 
