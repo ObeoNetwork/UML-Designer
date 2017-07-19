a50a62d04007fbb983fbeac9e485fe9ef876c542
Mélanie Bats <melanie.bats@obeo.fr>
2017-07-19 10:38:59 +0200

    Fix #952 Use the new version of the activity explorer.
    Signed-off-by: Mélanie Bats <melanie.bats@obeo.fr>

--- plugins/org.obeonetwork.dsl.uml2.dashboard/src/org/obeonetwork/dsl/uml2/dashboard/commands/DashboardContributionItems.java
+++ plugins/org.obeonetwork.dsl.uml2.dashboard/src/org/obeonetwork/dsl/uml2/dashboard/commands/DashboardContributionItems.java
@@ -28,9 +28,11 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 /**
- * Contribute to the open dashboard toolbar the different available dashboards per project name.
+ * Contribute to the open dashboard toolbar the different available dashboards
+ * per project name.
  *
- * @author Melanie Bats <a href="mailto:melanie.bats@obeo.fr">melanie.bats@obeo.fr</a>
+ * @author Melanie Bats
+ *         <a href="mailto:melanie.bats@obeo.fr">melanie.bats@obeo.fr</a>
  */
 public class DashboardContributionItems extends CompoundContributionItem {
 	/**
@@ -64,22 +66,24 @@ public class DashboardContributionItems extends CompoundContributionItem {
 		// Get all available dashboards
 		final List<EObject> umlModelRoots = DashboardServices.INSTANCE.getUmlModels();
 		for (final EObject eObject : umlModelRoots) {
-			// Get the project name
-			final IFile resourceFile = ResourcesPlugin.getWorkspace().getRoot()
-					.getFile(new Path(eObject.eResource().getURI().toPlatformString(true)));
-			final String projectName = resourceFile.getProject().getName();
-			final Map<String, String> parameters = Maps.newHashMap();
-			parameters.put(OPEN_DASHBOARD_CMD_PARAM_MODEL_KEY, projectName);
-			final CommandContributionItemParameter contributionParameter = new CommandContributionItemParameter(
-					PlatformUI.getWorkbench(), null, OPEN_DASHBOARD_CMD_ID, parameters, null, null, null,
-					projectName, null, "Open the activity explorer of the project : " + projectName, //$NON-NLS-1$
-					CommandContributionItem.STYLE_PUSH, null, true);
+			String uri = eObject.eResource().getURI().toPlatformString(true);
+			if (uri != null) {
+				// Get the project name
+				final IFile resourceFile = ResourcesPlugin.getWorkspace().getRoot().getFile(new Path(uri));
+				final String projectName = resourceFile.getProject().getName();
+				final Map<String, String> parameters = Maps.newHashMap();
+				parameters.put(OPEN_DASHBOARD_CMD_PARAM_MODEL_KEY, projectName);
+				final CommandContributionItemParameter contributionParameter = new CommandContributionItemParameter(
+						PlatformUI.getWorkbench(), null, OPEN_DASHBOARD_CMD_ID, parameters, null, null, null,
+						projectName, null, "Open the activity explorer of the project : " + projectName, //$NON-NLS-1$
+						CommandContributionItem.STYLE_PUSH, null, true);
 
-			contributionParameter.visibleEnabled = true;
-			// Create a new item in the menu for the project
-			final CommandContributionItem commandContributionItem = new CommandContributionItem(
-					contributionParameter);
-			menuItems.add(commandContributionItem);
+				contributionParameter.visibleEnabled = true;
+				// Create a new item in the menu for the project
+				final CommandContributionItem commandContributionItem = new CommandContributionItem(
+						contributionParameter);
+				menuItems.add(commandContributionItem);
+			}
 		}
 
 		return menuItems.toArray(new IContributionItem[menuItems.size()]);
