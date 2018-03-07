738f154ced42e28c712de3e5fb2494188aefe01c
Frederic Bats <frederic.bats@obeo.fr>
2018-03-07 09:37:21 +0100

    Rename nightly update site for uml2.core

--- prepare-deploy-local-dir.sh
+++ prepare-deploy-local-dir.sh
@@ -2,16 +2,19 @@
 CORE_UPDATE_SITE_DIR=packaging/org.obeonetwork.dsl.uml2.core.update/target/
 UPDATE_SITE_DIR=packaging/org.obeonetwork.dsl.uml2.update/target/
 TP=releng/org.obeonetwork.dsl.uml2.target/*.tpd
+CORE_TP=releng/org.obeonetwork.dsl.uml2.core.target/*.tpd
 BUNDLE_DIR=packaging/org.obeonetwork.dsl.uml2.product/target/products/*.zip
 DEPLOY_LOCAL_DIR=$1
 echo "Prepare deploy local dir = ${DEPLOY_LOCAL_DIR}"
 # Create nightly folder
 mkdir $DEPLOY_LOCAL_DIR 
+mkdir $DEPLOY_LOCAL_DIR/core 
 # Copy update-site and target platform to deploy local dir
-cp -r $CORE_UPDATE_SITE_DIR/repository $DEPLOY_LOCAL_DIR
+cp -r $CORE_UPDATE_SITE_DIR/repository $DEPLOY_LOCAL_DIR/core
 cp -r $CORE_UPDATE_SITE_DIR/*.zip $DEPLOY_LOCAL_DIR
 cp -r $UPDATE_SITE_DIR/repository $DEPLOY_LOCAL_DIR
 cp -r $UPDATE_SITE_DIR/*.zip $DEPLOY_LOCAL_DIR
+cp -r $CORE_TP $DEPLOY_LOCAL_DIR
 cp -r $TP $DEPLOY_LOCAL_DIR
 cp -r UMLDesigner.zip $DEPLOY_LOCAL_DIR
 echo "ls ${DEPLOY_LOCAL_DIR}"
